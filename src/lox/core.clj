f0b1b2657887eede08ee1437034aae244157ee57
Jim Cheung <usr.jim@gmail.com>
2021-08-13 20:43:05 +0800

    update

--- src/lox/core.clj
+++ src/lox/core.clj
@@ -1,7 +1,23 @@
 (ns lox.core
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def had-error (atom false))
+
+(defn report [line where message]
+  (println (format "[line %d] Error %s: %s" line where message))
+  (reset! had-error true))
+
+(defn error [line message]
+  (report line "" message))
+
+(defn run [source]
+  (if @had-error
+    (println "error, exit here")
+    (println (format "pass %s to scanner" source))))
+
+(defn run-file [path]
+  (-> path slurp run))
+
+(defn -main [& args]
+  (println "Usage: clox [script]"))
